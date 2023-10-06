2cfd2e5c93c76a3f0320841719850363074e68da
FireFart1204 <130674991+FireFart1204@users.noreply.github.com>
2023-10-07 01:46:16 +0530

    Update position_hold_pid.py

--- position_hold_pid.py
+++ position_hold_pid.py
@@ -58,9 +58,9 @@ class swift():
 		#initial setting of Kp, Kd and ki for [roll, pitch, throttle]. eg: self.Kp[2] corresponds to Kp value in throttle axis
 		#after tuning and computing corresponding PID parameters, change the parameters
 
-		self.Kp = [5.358, 8.52, 180.26]
-		self.Ki = [0, 0, 0.0126]
-		self.Kd = [11.18, 0, 990.8]
+		self.Kp = [5.358, 8.52, 21.84]
+		self.Ki = [0, 0, 0.0112]
+		self.Kd = [11.18, 0, 596.1]
    
 		#-----------------------Add other required variables for pid here ----------------------------------------------
 
@@ -180,7 +180,7 @@ class swift():
 		
 
 	def roll_set_pid(self,roll):
-		self.Kp[0] = roll.Kp * 0.06 # This is just for an example. You can change the ratio/fraction value accordingly
+		self.Kp[0] = roll.Kp * 0.02 # This is just for an example. You can change the ratio/fraction value accordingly
 		self.Ki[0] = roll.Ki * 0.0008
 		self.Kd[0] = roll.Kd * 0.3
 	def pitch_set_pid(self,pitch):
@@ -228,7 +228,7 @@ class swift():
 			self.pitch_error =  self.drone_position[1] - self.setpoint[1]
 			self.roll_error = self.drone_position[0] - self.setpoint[0]
 		
-			self.cmd.rcThrottle = int(1570 + (self.alt_error*self.Kp[2]) + ((self.alt_error - self.prev_alt_error)*self.Kd[2]) + (self.sum_alt_error+self.alt_error)*self.Ki[2])
+			self.cmd.rcThrottle = int(1572 + (self.alt_error*self.Kp[2]) + ((self.alt_error - self.prev_alt_error)*self.Kd[2]) + (self.sum_alt_error+self.alt_error)*self.Ki[2])
 			self.cmd.rcPitch = int(1500 + (self.pitch_error*self.Kp[1]) + ((self.pitch_error - self.prev_pitch_error)*self.Kd[1]) + (self.sum_pitch_error)*self.Ki[1])
 			self.cmd.rcRoll = int(1500 - (self.roll_error*self.Kp[0]) - ((self.roll_error - self.prev_roll_error)*self.Kd[0]) )
 
@@ -305,5 +305,6 @@ if __name__ == '__main__':
 	while not rospy.is_shutdown():
 		swift_drone.pid()
 		r.sleep()
+
 
 
